e847691d991ac6109b8750266f7372ef275edf85
alecsimone <alecsimone@gmail.com>
2022-05-06 16:57:58 -0600

    renamed foundational directory to foundation

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
-import Layout from '../components/foundational/Layout';
+import Layout from '../components/foundation/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Layout><Component {...pageProps} /></Layout>;
