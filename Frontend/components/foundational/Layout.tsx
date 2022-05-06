8f0da87214b316cc338999d6e835bd50c2b2104a
alecsimone <alecsimone@gmail.com>
2022-05-05 18:19:26 -0600

    Setup nprogress

--- Frontend/components/foundational/Layout.tsx
+++ Frontend/components/foundational/Layout.tsx
@@ -1,8 +1,10 @@
-import { FC, ReactNode } from 'react';
+import Router from 'next/router';
+import { FC, ReactNode, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
+import NProgress from 'nprogress';
 import GlobalStyle from '../../styles/globalStyle';
 import theme from '../../styles/theme';
-import Header from './Header';
+import Header from './Header/Header';
 import Meta from './Meta';
 
 interface LayoutProps {
@@ -10,7 +12,21 @@ interface LayoutProps {
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  console.log('Layout');
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on('routeChangeStart', handleRouteStart);
+    Router.events.on('routeChangeComplete', handleRouteDone);
+    Router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off('routeChangeStart', handleRouteStart);
+      Router.events.off('routeChangeComplete', handleRouteDone);
+      Router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
